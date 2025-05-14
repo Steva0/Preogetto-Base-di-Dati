a8d23f6df6f497bb7b08222e426ac95cd994bdc3
Steva <148330069+Steva0@users.noreply.github.com>
2025-05-14 23:09:44 +0200

    Update Crociere.sql

--- Crociere.sql
+++ Crociere.sql
@@ -101,3 +101,48 @@ CREATE TABLE ORGANIZZA (
     FOREIGN KEY (Tipologia_Evento) REFERENCES Evento(Tipologia),
     FOREIGN KEY (IMO_Crociera_Evento) REFERENCES Evento(IMO_Crociera)
 );
+
+-- Query 1 – Trovare le crociere che toccano più di 3 porti diversi e indicarne la città di partenza, di arrivo e il numero di tappe
+SELECT C.IMO, C.Nome_Nave, C.Porto_Partenza, C.Porto_Finale, COUNT(DISTINCT T.Città) AS Numero_Tappe
+FROM Crociera C
+JOIN Tappa T ON C.IMO = T.IMO
+GROUP BY C.IMO, C.Nome_Nave, C.Porto_Partenza, C.Porto_Finale
+HAVING COUNT(DISTINCT T.Città) > 3
+ORDER BY Numero_Tappe DESC;
+
+-- QUERY 2 – Trovare per ogni città (porto), quante crociere partono da lì e la media del numero di prenotazioni
+SELECT p.Città, COUNT(c.IMO) AS Numero_Crociere, AVG(c.Num_Prenotazioni) AS Media_Prenotazioni
+FROM Porto p
+JOIN Crociera c ON p.Città = c.Porto_Partenza
+GROUP BY p.Città
+ORDER BY Numero_Crociere DESC;
+
+-- Query 3 – Trovare le crociere che hanno una media del costo dei biglietti superiore a 500 euro
+SELECT O.IMO_Crociera, C.Nome_Nave, AVG(O.Costo) AS Media_Costo
+FROM Ospite O
+JOIN Crociera C ON O.IMO_Crociera = C.IMO
+GROUP BY O.IMO_Crociera, C.Nome_Nave
+HAVING AVG(O.Costo) > 500
+ORDER BY Media_Costo DESC;
+
+-- Query 4 – Visualizzare gli animatori con più eventi organizzati, mostrando solo quelli con più di 2 eventi
+SELECT A.CF, P.Nome, P.Cognome, COUNT(*) AS Num_Eventi
+FROM Animatore A
+JOIN Persona P ON A.CF = P.CF
+JOIN ORGANIZZA O ON A.CF = O.CF_Animatore
+GROUP BY A.CF, P.Nome, P.Cognome
+HAVING COUNT(*) > 2
+ORDER BY Num_Eventi DESC;
+
+-- Query 5 – Trovare, per ogni crociera, la percentuale di occupazione rispetto alla capacità massima (Num_Prenotazioni / Max_Passeggeri)
+SELECT IMO, Nome_Nave,
+       ROUND((Num_Prenotazioni * 100.0) / Max_Passeggeri, 2) AS Percentuale_Occupazione
+FROM Crociera
+WHERE Max_Passeggeri > 0
+ORDER BY Percentuale_Occupazione DESC;
+
+-- INDICE PER QUERY 2
+CREATE INDEX idx_crociera_porto_partenza ON Crociera(Porto_Partenza);
+-- La query accede frequentemente alla tabella Crociera filtrando per Porto_Partenza (colonna che rappresenta una foreign key verso Porto(Città)), per poi eseguire un join con Porto e una successiva aggregazione.
+-- L’indice creato su Crociera(Porto_Partenza) consente una ricerca più veloce di tutte le crociere con partenza da un certo porto, 
+-- un’efficiente costruzione del join tra Crociera e Porto e una rapida scansione per il GROUP BY che raggruppa proprio per città.
